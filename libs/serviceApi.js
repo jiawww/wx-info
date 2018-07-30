22a59e0527f7297c827996511b808e47be7b1327
jiawww <zjemail2017@163.com>
2018-07-30 22:51:27 +0800

    123

--- libs/serviceApi.js
+++ libs/serviceApi.js
@@ -1,9 +1,12 @@
-const serviceApi={
-  AddPublishMessage:"http://47.93.45.17:8080/WeixinPro/JobHuntingServlet?method=AddPublishMessage",
-  UpdatePublishMessage:"",
-  PublishMessagesList:"",
-  DeleteMessage:"",
-  UserLogin:"",
-  ShowUserInfo:""
+const url ="http://47.93.45.17:8080/WeixinPro/";
+const serviceApi={  
+  AddPublishMessage:url+"JobHuntingServlet?method=AddPublishMessage",
+  UpdatePublishMessage:url+"JobHuntingServlet?method=UpdatePublishMessage",
+  PublishMessagesList: url +"JobHuntingServlet?method=PublishMessagesList",
+  DeleteMessage: url +"JobHuntingServlet?method=DeleteMessage",
+  UserLogin: url +"JobHuntingServlet?method=UserLogin",
+  ShowUserInfo: url +"JobHuntingServlet?method=ShowUserInfo",
+  RequestHumanCheck: url +"JobHuntingServlet?method=RequestHumanCheck",
+  ModifyTelephoneNumber: url +"JobHuntingServlet?method=ModifyTelephoneNumber"
 }
 module.exports=serviceApi;
